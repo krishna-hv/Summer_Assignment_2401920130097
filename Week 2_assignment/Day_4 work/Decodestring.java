4252974a3de3e265a9370ea9d63fba18d15f519f
Krishna Varshney <krishnavarshneyhv@gmail.com>
2026-06-11 21:34:08 +0530

    Create Decodestring.java

---
+++ Week 2_assignment/Day_4 work/Decodestring.java
@@ -0,0 +1,37 @@
+class Solution {
+    int index = 0;
+
+    public String decodeString(String s) {
+        StringBuilder result = new StringBuilder();
+
+        while (index < s.length() && s.charAt(index) != ']') {
+
+            char ch = s.charAt(index);
+
+            if (Character.isLetter(ch)) {
+                result.append(ch);
+                index++;
+            } else if (Character.isDigit(ch)) {
+                int num = 0;
+
+                while (index < s.length() &&
+                       Character.isDigit(s.charAt(index))) {
+                    num = num * 10 + (s.charAt(index) - '0');
+                    index++;
+                }
+
+                index++; 
+
+                String decodedPart = decodeString(s);
+
+                index++; 
+
+                for (int i = 0; i < num; i++) {
+                    result.append(decodedPart);
+                }
+            }
+        }
+
+        return result.toString();
+    }
+}
