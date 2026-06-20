fc553478b3c20610de1e9a1b42cb0ea3585c97d0
Krishna Varshney <krishnavarshneyhv@gmail.com>
2026-06-20 22:15:26 +0530

    Create FirstClass.java

---
+++ Week_3/OOPS /FirstClass.java
@@ -0,0 +1,10 @@
+package Week_3.OOPS;
+
+class FirstClass extends Compartment {
+
+    @Override
+    public String notice() {
+        return "First Class Compartment: Reserved for First Class passengers.";
+    }
+
+}
