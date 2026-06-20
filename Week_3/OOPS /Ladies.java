b157be030796cb092c82d31e8bd5b18beb498bff
Krishna Varshney <krishnavarshneyhv@gmail.com>
2026-06-20 22:17:01 +0530

    Add Ladies class for women passenger compartment

---
+++ Week_3/OOPS /Ladies.java
@@ -0,0 +1,10 @@
+package Week_3.OOPS;
+
+class Ladies extends Compartment {
+
+    @Override
+    public String notice() {
+        return "Ladies Compartment: Reserved for women passengers.";
+    }
+
+}
