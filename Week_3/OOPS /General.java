61ecd6ffd52fc816a581392d676900fe069292ce
Krishna Varshney <krishnavarshneyhv@gmail.com>
2026-06-20 22:16:13 +0530

    Add General class extending Compartment

---
+++ Week_3/OOPS /General.java
@@ -0,0 +1,10 @@
+package Week_3.OOPS;
+
+class General extends Compartment {
+
+    @Override
+    public String notice() {
+        return "General Compartment: Open for all passengers.";
+    }
+
+}
